01355be2ced992e199ff7affee400b2b3c21b473
Derek Loayza <derek.loayza@gmail.com>
2026-05-17 18:38:18 -0500

    fear:Creacion de la tabla Proveedor

--- scripts_sql/01_ddl_estructura.sql
+++ scripts_sql/01_ddl_estructura.sql
@@ -14,3 +14,13 @@ CREATE TABLE cliente (
     email VARCHAR(100) UNIQUE,
     direccion VARCHAR(200)
 );
+
+--Segunda entidad: Proveedor
+CREATE TABLE proveedor (
+    id_prov SERIAL PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    ruc VARCHAR(11) NOT NULL UNIQUE,
+    telefono VARCHAR(20),
+    email VARCHAR(100),
+    direccion VARCHAR(200)
+);
