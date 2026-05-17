75dd3ddb6efae80fd30a82c68fcb0f4377a5cb4e
Derek Loayza <derek.loayza@gmail.com>
2026-05-17 18:37:41 -0500

    feat: Creacion de la tabla Cliente

--- scripts_sql/01_ddl_estructura.sql
+++ scripts_sql/01_ddl_estructura.sql
@@ -3,3 +3,14 @@ CREATE TABLE cargo (
     id_cargo SERIAL PRIMARY KEY,
     nombre VARCHAR(50) NOT NULL UNIQUE
 ); 
+
+--Segunda entidad: Cliente
+CREATE TABLE cliente (
+    id_cli SERIAL PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    apellido VARCHAR(50) NOT NULL,
+    dni VARCHAR(15) NOT NULL UNIQUE,
+    telefono VARCHAR(20),
+    email VARCHAR(100) UNIQUE,
+    direccion VARCHAR(200)
+);
