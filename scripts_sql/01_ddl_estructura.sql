4dcbf2365285eccc7725009bd0d167994a7c23dc
Rafael Imanol Gomero Mendoza <155471048+RafaelSoftDev@users.noreply.github.com>
2026-05-18 09:27:14 -0500

    feat: Creacion de la tabla 5: Smartphone

--- scripts_sql/01_ddl_estructura.sql
+++ scripts_sql/01_ddl_estructura.sql
@@ -26,17 +26,37 @@ CREATE TABLE proveedor (
 );
 
 -- Tercera entidad: Categoría
-CREATE TABLE Categoria{
+CREATE TABLE categoria{
     id_cat INT PRIMARY KEY,
     nombre VARCHAR(50) NOT NULL UNIQUE
 };
 
 -- Cuarta entidad: Marca
-CREATE TABLE Marca{
+CREATE TABLE marca{
     id_marca INT PRIMARY KEY,
     nombre VARCHAR(50) NOT NULL UNIQUE
 };
 
+-- Quinta entidad: Smartphone
+CREATE TABLE smartphone{
+    id_smart SERIAL PRIMARY KEY,
+    modelo VARCHAR(100) NOT NULL,
+    precio DECIMAL(10,2) NOT NULL,
+    stock INT,
+    especificaciones TEXT,
+    -- Utilización de claves foráneas
+    -- Claves de la tabla marca y categoria
+    -- Formas corta
+    -- id_marca INT REFERENCES marca(id_marca),
+    -- id_cat INT REFERENCES categoria(id_cat)
+    -- Forma correcta
+    id_marca INT,
+    id_cat INT,
+    CONSTRAINT fk_smartphone_marca 
+        FOREIGN KEY (id_marca) REFERENCES marca(id_marca),
+    CONSTRAINT fk_smartphone_categoria 
+        FOREIGN KEY (id_cat) REFERENCES categoria(id_cat)
+};
 
 --Novena entidad: Employees
 CREATE TABLE employees(
