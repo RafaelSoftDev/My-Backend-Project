a847b8c807ba9d4d3dee99fa932a2d316e53b581
JorgeMamaniEsc <152026996+JorgeMamaniEsc@users.noreply.github.com>
2026-05-22 09:29:09 -0500

    Fix: Nombre de id_suc

--- scripts_sql/01_ddl_estructura.sql
+++ scripts_sql/01_ddl_estructura.sql
@@ -60,7 +60,7 @@ CREATE TABLE smartphone{
 
 -- Sexta entidad: Sucursal
 CREATE TABLE Sucursal{
-    id_sue SERIAL PRIMARY KEY,
+    id_suc SERIAL PRIMARY KEY,
     nombre VARCHAR(50) NOT NULL UNIQUE,
     direccion VARCHAR(100) NOT NULL UNIQUE,
     telefono VARCHAR(20) NOT NULL UNIQUE
