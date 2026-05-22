9ca64bd6f4f852c8acbbdb91080978aa40c1f8b1
JorgeMamaniEsc <152026996+JorgeMamaniEsc@users.noreply.github.com>
2026-05-22 09:21:31 -0500

    Feat: Poblando tabla employees
    Insert multiple records into the employees table.

---
+++ scripts_sql/03_dml_poblado_datos.sql
@@ -0,0 +1,9 @@
+
+--Poblando tabla employees
+INSERT INTO employees(nombre,apellido,dni)
+VALUES
+	('Ana','Vargas',55632552),
+	('Juan','Flores',55332552),
+	('Dario','Guillen',55932552),
+	('Dante','Lopez',55932552),	
+	('Jorge','Justo',55832552);
