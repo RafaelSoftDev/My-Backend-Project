b65c441f8c1eb67abef8db53242f2d1ec1a1a657
JorgeMamaniEsc <152026996+JorgeMamaniEsc@users.noreply.github.com>
2026-05-22 09:22:19 -0500

    feat: poblando tabla cabecera_venta

--- scripts_sql/03_dml_poblado_datos.sql
+++ scripts_sql/03_dml_poblado_datos.sql
@@ -7,3 +7,14 @@ VALUES
 	('Dario','Guillen',55932552),
 	('Dante','Lopez',55932552),	
 	('Jorge','Justo',55832552);
+
+
+--Poblando tabla cabecera_venta
+INSERT INTO cabecera_venta(fecha,total)
+VALUES
+	('2026-05-20',12),
+	('2026-05-20',11),
+	('2026-05-20',29),	
+	('2026-03-21',10),
+	('2026-03-22',23);
+
