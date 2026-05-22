d9fc738d6ffad39afffc3f436a9860dff063e8e5
ALUMNO D102 <ALUMNO-D102@UCSP.AULAB>
2026-05-22 09:57:36 -0500

    feat: Poblando table proveedor

--- scripts_sql/03_dml_poblado_datos.sql
+++ scripts_sql/03_dml_poblado_datos.sql
@@ -47,3 +47,14 @@ VALUES
 UPDATE employees
 SET id_suc=1
 WHERE id_emp=2;
+
+
+
+--Poblando tabla metodo_pago
+INSERT INTO proveedor(nombre,ruc,telefono,email,direccion)
+VALUES 
+('Bao import',36584612683,957281463,'bao.import67@gmail.com','Shanghái' ),
+('Qiang import',57149376105,914672083,'qiang.import57@gmail.com','Shanghái '),
+('Shangdu import',94712947262,961872034,'shangdu.import41@gmail.com','Shanghái '),
+('Chang import',19295725492,994719853,'chang.import95@gmail.com','Shanghái '),
+('Ming import',20586283720,940274876,'ming.import26@gmail.com','Shanghái ');
