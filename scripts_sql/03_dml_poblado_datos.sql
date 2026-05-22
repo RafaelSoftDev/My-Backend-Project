ab969edf55ce9a6481a9f3f2a4a3caf2804e1192
JorgeMamaniEsc <152026996+JorgeMamaniEsc@users.noreply.github.com>
2026-05-22 09:40:55 -0500

    Fix: Arreglando insert en employees

--- scripts_sql/03_dml_poblado_datos.sql
+++ scripts_sql/03_dml_poblado_datos.sql
@@ -6,15 +6,21 @@ VALUES
     ('Pedidos SAC', 'Parque Industrial N2', '994720860'),
     ('Fast SAC', 'Av. Arequipa 333', '954108721'),
     ('International SAC', 'Parque Industrial N3', '978410635')
-	
+
 --Poblando tabla employees
-INSERT INTO employees(nombre,apellido,dni)
+INSERT INTO employees(nombre,apellido,dni,id_suc,id_cargo)
 VALUES
-	('Ana','Vargas',55632552),
-	('Juan','Flores',55332552),
-	('Dario','Guillen',55932552),
-	('Dante','Lopez',55932552),	
-	('Jorge','Justo',55832552);
+	('Ana','Vargas',55632552,(SELECT id_suc FROM Sucursal WHERE nombre = 'Fong SAC'), 
+        (SELECT id_cargo FROM cargo WHERE nombre = 'Administrador')),
+	('Juan','Flores',55332552,(SELECT id_suc FROM Sucursal WHERE nombre = 'International SAC'), 
+        (SELECT id_cargo FROM cargo WHERE nombre = 'Administrador')),
+	('Dario','Guillen',55932552,(SELECT id_suc FROM Sucursal WHERE nombre = 'MiTienda SAC'), 
+        (SELECT id_cargo FROM cargo WHERE nombre = 'Administrador')),
+	('Dante','Lopez',55932552,(SELECT id_suc FROM Sucursal WHERE nombre = 'Fong SAC'), 
+        (SELECT id_cargo FROM cargo WHERE nombre = 'Administrador')),	
+	('Jorge','Justo',55832552,(SELECT id_suc FROM Sucursal WHERE nombre = 'Sucursal Centro'), 
+        (SELECT id_cargo FROM cargo WHERE nombre = 'Administrador'));
+
 
 
 --Poblando tabla metodo_pago
