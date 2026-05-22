6b5c640754907dfde27cc225e55177017940de45
Rafael Imanol Gomero Mendoza <155471048+RafaelSoftDev@users.noreply.github.com>
2026-05-22 09:36:32 -0500

    Add data population for sucursal

--- scripts_sql/03_dml_poblado_datos.sql
+++ scripts_sql/03_dml_poblado_datos.sql
@@ -1,4 +1,12 @@
-
+-- Poblando tabla sucursal
+INSERT INTO sucursal(nombre, direccion, telefono)
+VALUES
+    ('Fong SAC', 'Av. Lima 502', '959124780'),
+    ('MiTienda SAC', 'Av. Progreso 512', '963204785'),
+    ('Pedidos SAC', 'Parque Industrial N2', '994720860'),
+    ('Fast SAC', 'Av. Arequipa 333', '954108721'),
+    ('International SAC', 'Parque Industrial N3', '978410635')
+	
 --Poblando tabla employees
 INSERT INTO employees(nombre,apellido,dni)
 VALUES
