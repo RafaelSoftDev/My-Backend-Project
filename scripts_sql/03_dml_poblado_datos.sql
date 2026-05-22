ab6c95bd21bf9f11ef03da24ba87c6f1fc165d1e
Rafael Imanol Gomero Mendoza <155471048+RafaelSoftDev@users.noreply.github.com>
2026-05-22 09:42:46 -0500

    Poblaso tabla smartphone

--- scripts_sql/03_dml_poblado_datos.sql
+++ scripts_sql/03_dml_poblado_datos.sql
@@ -5,7 +5,16 @@ VALUES
     ('MiTienda SAC', 'Av. Progreso 512', '963204785'),
     ('Pedidos SAC', 'Parque Industrial N2', '994720860'),
     ('Fast SAC', 'Av. Arequipa 333', '954108721'),
-    ('International SAC', 'Parque Industrial N3', '978410635')
+    ('International SAC', 'Parque Industrial N3', '978410635');
+
+-- Poblando tabla smartphone
+INSERT INTO smartphone(modelo, precio, stock, especificaciones, id_marca, id_cat)
+VALUES
+    ('Samsung A7', 250.50, 50, 'Modelo media gama', 1, 2),
+    ('Xiaomi 2', 220.00, 40, 'Modelo media-alta gama', 2, 2),
+    ('ZTE Lite', 200.50, 100, 'Modelo medio-baja gama', 3, 3),
+    ('Iphone 17', 500.00, 20, 'Modelo alta gama', 4, 1),
+    ('LG Series 7', 180.50, 150, 'Modelo baja gama', 5, 4);
 
 --Poblando tabla employees
 INSERT INTO employees(nombre,apellido,dni,id_suc,id_cargo)
